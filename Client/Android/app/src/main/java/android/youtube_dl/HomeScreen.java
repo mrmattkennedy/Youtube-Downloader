60bde9efad482009462dc24d55316142b266c6da
Matthew Kennedy <35405371+mrmattkennedy@users.noreply.github.com>
2019-11-19 16:46:20 -0500

    Testing gitignore

---
+++ Client/Android/app/src/main/java/android/youtube_dl/HomeScreen.java
@@ -0,0 +1,48 @@
+package android.youtube_dl;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.inputmethod.EditorInfo;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class HomeScreen extends AppCompatActivity {
+
+    private EditText urlText;
+    private final Pattern pattern = Pattern.compile(
+            "http(?:s)?://(?:www\\.)?youtu(?:\\.be/|be\\.com/(?:watch\\?v=|v/|embed/|user/(?:[\\w#]+/)+))([^&#?\\n]+)",
+                    Pattern.CASE_INSENSITIVE);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_screen);
+
+        urlText = findViewById(R.id.urlInput);
+        urlText.setOnEditorActionListener(new EditText.OnEditorActionListener() {
+
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if (actionId == EditorInfo.IME_ACTION_SEARCH ||
+                        actionId == EditorInfo.IME_ACTION_DONE ||
+                        event != null &&
+                                event.getAction() == KeyEvent.ACTION_DOWN &&
+                                event.getKeyCode() == KeyEvent.KEYCODE_ENTER) {
+                    if (event == null || !event.isShiftPressed()) {
+                        String url = urlText.getText().toString();
+                        Matcher matcher = pattern.matcher(url);
+                        Log.d("TEST", Boolean.toString(matcher.matches()));
+
+                        return true; // consume.
+                    }
+                }
+                return false; // pass on to other listeners.
+            }
+        });
+
+    }
+}
