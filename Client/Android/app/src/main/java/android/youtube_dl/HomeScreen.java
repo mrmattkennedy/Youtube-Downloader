4319b7564a4e8f0756e925b41d47a055ee4d87fb
Matthew Kennedy <35405371+mrmattkennedy@users.noreply.github.com>
2019-11-19 18:08:42 -0500

    Added Youtube search API functionality and JSON parsing

--- Client/Android/app/src/main/java/android/youtube_dl/HomeScreen.java
+++ Client/Android/app/src/main/java/android/youtube_dl/HomeScreen.java
@@ -1,5 +1,6 @@
 package android.youtube_dl;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -8,8 +9,19 @@ import android.view.inputmethod.EditorInfo;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+
+import javax.net.ssl.HttpsURLConnection;
 
 public class HomeScreen extends AppCompatActivity {
 
@@ -17,6 +29,11 @@ public class HomeScreen extends AppCompatActivity {
     private final Pattern pattern = Pattern.compile(
             "http(?:s)?://(?:www\\.)?youtu(?:\\.be/|be\\.com/(?:watch\\?v=|v/|embed/|user/(?:[\\w#]+/)+))([^&#?\\n]+)",
                     Pattern.CASE_INSENSITIVE);
+    private URL searchUrl;
+    private HttpsURLConnection urlConnection;
+    private BufferedReader reader;
+    private StringBuilder buffer;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,7 +41,6 @@ public class HomeScreen extends AppCompatActivity {
 
         urlText = findViewById(R.id.urlInput);
         urlText.setOnEditorActionListener(new EditText.OnEditorActionListener() {
-
             @Override
             public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
                 if (actionId == EditorInfo.IME_ACTION_SEARCH ||
@@ -35,14 +51,64 @@ public class HomeScreen extends AppCompatActivity {
                     if (event == null || !event.isShiftPressed()) {
                         String url = urlText.getText().toString();
                         Matcher matcher = pattern.matcher(url);
-                        Log.d("TEST2", Boolean.toString(matcher.matches()));
 
+                        if (!matcher.matches())
+                        {
+                            new VideoSearch().execute();
+                        }
                         return true; // consume.
                     }
                 }
                 return false; // pass on to other listeners.
             }
         });
+    }
 
+    public class VideoSearch extends AsyncTask<Void,Void,String>
+    {
+        @Override
+        protected String doInBackground(Void... params) {
+            try {
+                searchUrl = new URL("https://www.googleapis.com/youtube/v3/search?key=" + getResources().getString(R.string.api_key) + "&maxResults=20&part=snippet&type=video&q=" + urlText.getText().toString());
+                urlConnection = (HttpsURLConnection) searchUrl.openConnection();
+                urlConnection.connect();
+
+                BufferedReader br = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+                buffer = new StringBuilder();
+                if (br == null) {
+                    return null;
+                }
+                String line;
+                while ((line = br.readLine()) != null) {
+                    buffer.append(line + "\n");
+                }
+
+                if (buffer.length() == 0)
+                    return null;
+
+                return buffer.toString();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String result)
+        {
+            try {
+                JSONObject objects = new JSONObject(result);
+                JSONArray jArray = objects.getJSONArray("items");
+                for(int i=0; i < jArray.length(); i++) {
+
+                    JSONObject jObject = jArray.getJSONObject(i);
+                    JSONObject snippet = jObject.getJSONObject(getResources().getString(R.string.video_info_tag));
+                    String test = "";
+
+                } // End Loop
+            } catch (JSONException e) {
+                Log.e("JSONException", "Error: " + e.toString());
+            } // catch (JSONException e)
+        }
     }
 }
